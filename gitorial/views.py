11dcd57819af7d325dedab7ecdf89856cba20f85
Oscar Bezi <oscar@bezi.io>
2014-09-06 06:31:32 -0400

    Added placeholder json from user API

--- gitorial/views.py
+++ gitorial/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import json
+
+def user(request, username):
+    response_data = {}
+    response_data['name'] = 'Tom Shen'
+    response_data['username'] = username
+    response_data['avatar_url'] = 'http://placekitten.com/400/400'
+    response_data['is_owner'] = True
+    response_data['tutorial'] = []
+    response_data['repos'] = []
+    
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
