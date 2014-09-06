fa07bed43c7f843c090ef7b0ad7390092b052c86
Justin Gallagher <justingallag@gmail.com>
2014-09-06 12:59:52 -0400

    Allow administration of database from admin page

--- gitorial/admin.py
+++ gitorial/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from gitorial.models import User, Tutorial, Step, Commit, File, Line
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Tutorial)
+admin.site.register(Step)
+admin.site.register(Commit)
+admin.site.register(File)
+admin.site.register(Line)
--- gitorial/models.py
+++ gitorial/models.py
@@ -4,27 +4,37 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, primary_key=True)
     avatar_url = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Tutorial(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     repo_url = models.CharField(max_length=100)
     owner = models.ForeignKey(User)
+    def __str__(self):
+        return self.title
 
 class Step(models.Model):
     title = models.CharField(max_length=100)
     content_before = models.CharField(max_length=500)
     content_after = models.CharField(max_length=500)
     tutorial = models.ForeignKey(Tutorial)
+    def __str__(self):
+        return self.title
 
 class Commit(models.Model):
     commit_url = models.CharField(max_length=100)
     code_url = models.CharField(max_length=100)
     step = models.OneToOneField(Step)
+    def __str__(self):
+        return commit_url
 
 class File(models.Model):
     commit = models.ForeignKey(Commit)
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return name
 
 class Line(models.Model):
     src_file = models.ForeignKey(File)
@@ -32,3 +42,5 @@ class Line(models.Model):
     content = models.CharField(max_length=200)
     addition = models.BooleanField()
     deletion = models.BooleanField()
+    def __str__(self):
+        return content
