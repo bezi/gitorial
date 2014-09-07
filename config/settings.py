3299088f07714ddc95c060ac11149442b42c5d72
Justin Gallagher <justingallag@gmail.com>
2014-09-07 01:14:43 -0400

    Set debug state in environment variables

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,6 @@ except:
 SECRET_KEY = '6=o8h-+6vt*7bwhn!wvj@+nan&p*hduv4*a)0-rt2%e09jgma_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['.gitorial.com', 'localhost', 'gitorial.herokuapp.com']
